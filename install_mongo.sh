5ac3973a6df0747aac71b3e43fc5fbeda1e5bb5b
Sayat <sayats@dallas.(none)>
2013-10-30 14:10:45 -0700

    added db dir to mongo isntalnce

--- install_mongo.sh
+++ install_mongo.sh
@@ -2,4 +2,7 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen
 sudo apt-get update 
-sudo apt-get install mongodb-10gen
+mkdir -p /usr/local/mongo/data/db/
+sudo apt-get install mongodb
+sudo service mongodb stop
+mongod --dbpath /usr/local/mongo/data/db
