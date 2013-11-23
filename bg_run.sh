316e83d189621bb15116387e715844e7b040a2d2
Sayat Stb <sayatez@gmail.com>
2013-11-23 12:40:16 -0800

    sharding changes

--- bg_run.sh
+++ bg_run.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
 BG_HOME="/usr/local/mongo/BGClient"
-
+TABLE_NAME="users"
 HOST_IP=10.0.0.240
-DB_NAME=bg_single
+DB_NAME=bg_single_shard
 THREAD_COUNT=10
 INSERT_IMAGE=true
 MAX_EXEC_TIME=600
-USER_COUNT=10000
+USER_COUNT=1000
 RES_PER_USER=100
 FRIEND_PER_USER=100
 
+dropDB(){
+   mongo $HOST_IP/$DB_NAME <<EOF
+   use $DB_NAME
+   db.dropDatabase();
+EOF
+}
 CreateSchema(){
     java -cp $BG_HOME/build/bg.jar:$BG_HOME/db/MongoDB/lib/* edu.usc.bg.base.Client \
         -schema -db mongoDB.MongoDbClient -p mongodb.url=$HOST_IP:27017 -p mongodb.database=$DB_NAME
@@ -19,6 +25,25 @@ CreateSchema(){
     if [ "$ret" -ne "0" ] 
     then
         echo "ERROR: in Creating Schema $ret"
+        exit 1
+    else
+        echo "DONE: $ret"
+    fi
+
+}
+
+ShardDB(){
+   mongo $HOST_IP/$DB_NAME <<EOF
+   sh.enableSharding("$DB_NAME");
+   use $DB_NAME
+   db.users.ensureIndex({ _id: "hashed"})
+   sh.shardCollection("$DB_NAME.$TABLE_NAME", {_id: "hashed"})
+EOF
+  
+   ret=$?
+    if [ "$ret" -ne "0" ]
+    then
+        echo "ERROR: in Sharding Schema $ret"
         exit 1
     else
         echo "DONE: $ret"
@@ -46,13 +71,16 @@ PopulateData(){
 
 }
 
+#    -p confperc=1 -p numloadthreads=$THREAD_COUNT \
 Workload(){
-    java -Xmx1024M -cp $BG_HOME/build/bg.jar:$BG_HOME/db/MongoDB/lib/* edu.usc.bg.base.Client \
+    java -cp $BG_HOME/build/bg.jar:$BG_HOME/db/MongoDB/lib/* edu.usc.bg.base.Client \
     -t -db mongoDB.MongoDbClient \
     -P $BG_HOME/workloads/SymmetricHighUpdateActions -s \
     -p mongodb.url=$HOST_IP:27017 -p threadcount=$THREAD_COUNT -p mongodb.writeConcern=strict \
-    -p mongodb.database=$DB_NAME -p exportfile=thread0.10.07.txt -p ratingmode=false \
-    -p maxexecutiontime=$MAX_EXEC_TIME -p initapproach=querydata -p usercount=$USER_COUNT
+    -p resourcecountperuser=$RES_PER_USER -p friendcountperuser=$FRIEND_PER_USER -p usercount=$USER_COUNT \
+    -p mongodb.database=$DB_NAME \
+    -p useroffset=0 -p confperc=1 -p numloadthreads=$THREAD_COUNT \
+    -p maxexecutiontime=$MAX_EXEC_TIME -p initapproach=deterministic -p exportfile=fileexport.txt
 
     ret=$?
     if [ "$ret" -ne "0" ] 
@@ -64,12 +92,17 @@ Workload(){
     fi
 
 }
+echo "****** Drop DB ******"
+#dropDB
 
 echo "****** Creating Schema ******"
-CreateSchema
+#CreateSchema
+
+echo "****** Sharding Schema ******"
+#ShardDB
 
 echo "****** Populating Data ******"
-PopulateData
+#PopulateData
 
 echo "****** Benchmarking  ********"
 Workload
--- install_mongo.sh
+++ install_mongo.sh
@@ -2,7 +2,4 @@
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
 sudo apt-get update 
-mkdir -p /usr/local/mongo/data/db/
 sudo apt-get install mongodb-10gen
-sudo service mongodb stop
-nohup mongod --dbpath /usr/local/mongo/data/db
