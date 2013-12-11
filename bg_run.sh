313a1837e39211bef237f6a10a88b74a33d6b67f
dblab-230 <dblab@dblab-130.(none)>
2013-12-10 21:05:01 -0800

    added write concern

--- bg_run.sh
+++ bg_run.sh
@@ -2,22 +2,25 @@
 
 BG_HOME="/home/dblab/mongo/BG"
 
-DROP_DB=0
-SHARD=0
-SCHEMA=0
-POP_DATA=0
+DROP_DB=1
+SHARD=1
+SCHEMA=1
+POP_DATA=1
 WORK=1
 
 SHARD_TABLE="users"
+SHARD_RES="resources"
 HOST_IP=10.0.0.130
-PORT=37017
-DB_NAME=bg_shard100K
+PORT=27017
+DB_NAME=bg2_shardUR_hash10K
 THREAD_COUNT=10
 INSERT_IMAGE=false
-MAX_EXEC_TIME=300
-USER_COUNT=100000
+MAX_EXEC_TIME=100
+USER_COUNT=10000
 RES_PER_USER=10
 FRIEND_PER_USER=10
+WORKLOAD=SymmetricVeryLowUpdateActions
+WRITECONCERN=normal
 
 dropDB(){
    mongo $HOST_IP:$PORT/$DB_NAME <<EOF
@@ -42,12 +45,22 @@ CreateSchema(){
 }
 
 ShardDB(){
-   mongo $HOST_IP:$PORT/$DB_NAME <<EOF
+   #db.users.ensureIndex({ _id: "hashed"})
+   #sh.shardCollection("$DB_NAME.$SHARD_TABLE", {_id: "hashed"})
+mongo $HOST_IP:$PORT/$DB_NAME <<EOF
    sh.enableSharding("$DB_NAME");
    use $DB_NAME
    db.users.ensureIndex({ _id: "hashed"})
    sh.shardCollection("$DB_NAME.$SHARD_TABLE", {_id: "hashed"})
+   sh.shardCollection("$DB_NAME.resources", {walluserid:"hashed"})
+
 EOF
+
+#mongo $HOST_IP:$PORT/$DB_NAME <<EOF
+#   sh.enableSharding("$DB_NAME");
+#   use $DB_NAME
+#   sh.shardCollection("$DB_NAME.$SHARD_TABLE", {_id: 1})
+#EOF
   
    ret=$?
     if [ "$ret" -ne "0" ]
@@ -67,7 +80,7 @@ PopulateData(){
     java -cp $BG_HOME/build/bg.jar:$BG_HOME/db/MongoDB/lib/* edu.usc.bg.BGMainClass \
     onetime -loadindex -db mongoDB.MongoDbClient \
     -p mongodb.url=$HOST_IP:$PORT -p insertimage=$INSERT_IMAGE -p threadcount=$THREAD_COUNT \
-    -p mongodb.writeConcern=strict -p mongodb.database=$DB_NAME \
+    -p mongodb.writeConcern=$WRITECONCERN -p mongodb.database=$DB_NAME \
     -p usercount=$USER_COUNT \
     -p resourcecountperuser=$RES_PER_USER -p friendcountperuser=$FRIEND_PER_USER \
     -P $BG_HOME/workloads/populateDB
@@ -86,12 +99,13 @@ PopulateData(){
 Workload(){
     java -Xmx10G -cp $BG_HOME/build/bg.jar:$BG_HOME/db/MongoDB/lib/* edu.usc.bg.BGMainClass \
     onetime -t -db mongoDB.MongoDbClient \
-    -P $BG_HOME/workloads/HighUpdateActions -s \
-    -p mongodb.url=$HOST_IP:$PORT -p threadcount=$THREAD_COUNT -p mongodb.writeConcern=strict \
+    -P $BG_HOME/workloads/$WORKLOAD -s \
+    -p mongodb.url=$HOST_IP:$PORT -p threadcount=$THREAD_COUNT -p mongodb.writeConcern=$WRITECONCERN \
     -p mongodb.database=$DB_NAME -p exportfile=thread0.10.07.txt -p ratingmode=false \
     -p maxexecutiontime=$MAX_EXEC_TIME -p initapproach=deterministic -p usercount=$USER_COUNT \
     -p resourcecountperuser=$RES_PER_USER -p friendcountperuser=$FRIEND_PER_USER \
     -p numloadthreads=10 \
+    -p warmup=1000 \
     -P $BG_HOME/workloads/populateDB
 
     ret=$?
